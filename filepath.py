f51b1914d6751d3dd82cac1e3c9430ce0bd3380f
Drishti Gehi <39626449+drish97@users.noreply.github.com>
2019-05-22 23:32:27 +0530

    Update filepath.py

--- filepath.py
+++ filepath.py
@@ -1,5 +1,14 @@
+#This is for python 3 and above
 from tkinter import *
-from tkinter import filedialog #for python 3.5
+#from tkinter import filedialog #for python 3.5
 root = Tk()
 root.filename=filedialog.askopenfilename( filetypes = ("All files","*.*") )
 print(root.fileName)
+
+#for python 2.7
+#from Tkinter import *from Tkinter import *
+#import Tkinter, Tkconstants, tkFileDialog
+
+#root = Tk()
+#root.filename = tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+#print (root.filename)
